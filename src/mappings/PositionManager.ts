836f13e777a437c900cee1915e01c6e6df12fef7
Roberto Martinez <63836051+Pochetes@users.noreply.github.com>
2022-08-31 22:24:16 -0400

    primary fixes (PR 2)

--- src/mappings/PositionManager.ts
+++ src/mappings/PositionManager.ts
@@ -1,11 +1,11 @@
 import { CreateLoan } from '../../generated/PositionManager/PositionManager'
-import { CreateLoanEntity } from '../../generated/schema'
+import { Loan } from '../../generated/schema'
 
 export function handleCreateLoan(event: CreateLoan): void {
-  let loan = CreateLoanEntity.load(event.params.tokenId.toString())
+  let loan = Loan.load(event.params.tokenId.toString())
 
   if (!loan) {
-    loan = new CreateLoanEntity(event.params.tokenId.toString()) 
+    loan = new Loan(event.params.tokenId.toString()) 
     loan.pool = event.params.pool
     loan.tokenId = event.params.tokenId
   }

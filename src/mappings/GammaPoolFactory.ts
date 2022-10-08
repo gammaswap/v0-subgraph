7151473a9a6c8f0ab586be3dcf7f4f1abe4df63e
Roberto Martinez <robertomiguel2001@gmail.com>
2022-10-08 04:21:41 -0400

    feat(mappings): create mappings for pool creation
    not all properties from the entity are accurate, using dummy data for now

--- src/mappings/GammaPoolFactory.ts
+++ src/mappings/GammaPoolFactory.ts
@@ -1,19 +1,44 @@
-import { PoolCreated as PoolCreatedSchema } from '../../generated/schema'
-import { PoolCreated } from '../../generated/GammaPoolFactory/GammaPoolFactory'
-import { bigInt } from '@graphprotocol/graph-ts'
+import { PoolCreated, GammaPoolFactory } from '../../generated/GammaPoolFactory/GammaPoolFactory'
+import { GammaPool as GammaPoolContract } from '../../generated/GammaPoolFactory/GammaPool'
+import { Pool as PoolCreatedSchema } from '../../generated/schema'
+import { Address, log } from '@graphprotocol/graph-ts'
+import { BigInt, Bytes, ByteArray, crypto, ethereum, bigInt } from '@graphprotocol/graph-ts'
+import { ONE_BI, SIX_BI } from './helpers'
+
+function loadOrCreatePool(id: string): PoolCreatedSchema {
+  const pool = PoolCreatedSchema.load(id)
+  if (pool) return pool
+
+  return new PoolCreatedSchema(id)
+}
 
 export function handlePoolCreated(event: PoolCreated): void {
-  let poolCreated = PoolCreatedSchema.load(event.params.pool.toString())
 
-  if (!poolCreated) {
-    poolCreated = new PoolCreatedSchema(event.params.pool.toString())
+  let gammaPool = GammaPoolContract.bind(event.params.pool)
 
-    poolCreated.pool = event.params.pool
-    poolCreated.cfmm = event.params.cfmm
-    poolCreated.protocol = event.params.protocol
-    poolCreated.protocolId = bigInt.fromString(event.params.protocolId.toString())
-    poolCreated.count = event.params.count
-  }
+  // loads event details into 
+  const poolCreated = loadOrCreatePool(event.transaction.hash.toHex())
+
+  poolCreated.address = event.params.pool
+  poolCreated.cfmm = event.params.cfmm
+  poolCreated.protocolId = bigInt.fromString(event.params.protocolId.toString())
+  poolCreated.protocol = event.params.protocol
+  poolCreated.count = event.params.count
+  poolCreated.tokenBalances = [ONE_BI, SIX_BI]
+  poolCreated.lpTokenBalance = ONE_BI
+  poolCreated.lpTokenBorrowed = ONE_BI
+  poolCreated.lpTokenBorrowedPlusInterest = ONE_BI
+  poolCreated.lpTokenTotal = ONE_BI
+  poolCreated.borrowRate = ONE_BI
+  poolCreated.accFeeIndex = ONE_BI
+  poolCreated.lastFeeIndex = ONE_BI
+  poolCreated.lastCFMMFeeIndex = ONE_BI
+  poolCreated.lastCFMMInvariant = ONE_BI
+  poolCreated.lastCFMMTotalSupply = ONE_BI
+  poolCreated.lastBlockNumber = ONE_BI
+  poolCreated.totalSupply = ONE_BI
 
   poolCreated.save()
 }
+
+// handlePoolUpdated()
---
+++ src/mappings/helpers.ts
@@ -0,0 +1,11 @@
+import { BigInt, BigDecimal } from "@graphprotocol/graph-ts"
+
+export const ADDRESS_ZERO = '0x0000000000000000000000000000000000000000'
+export const UNISWAV2_FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5aA6f'
+
+export let ZERO_BI = BigInt.fromI32(0)
+export let ONE_BI = BigInt.fromI32(1)
+export let SIX_BI = BigInt.fromI32(6)
+export let ZERO_BD = BigDecimal.fromString('0')
+export let ONE_BD = BigDecimal.fromString('1')
+export let BI_18 = BigInt.fromI32(18)

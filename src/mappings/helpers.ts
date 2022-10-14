5352820a3a6f6695d5caad0a512395b25e748699
Roberto Martinez <robertomiguel2001@gmail.com>
2022-10-13 20:38:18 -0400

    refactor(manifest): setup pool update events

--- src/mappings/helpers.ts
+++ src/mappings/helpers.ts
@@ -6,6 +6,21 @@ export const UNISWAV2_FACTORY_ADDRESS = '0x5C69bEe701ef814a2B6a3EDD4B1652CB9cc5a
 export let ZERO_BI = BigInt.fromI32(0)
 export let ONE_BI = BigInt.fromI32(1)
 export let SIX_BI = BigInt.fromI32(6)
+export let SIX_BD = BigDecimal.fromString('6')
 export let ZERO_BD = BigDecimal.fromString('0')
 export let ONE_BD = BigDecimal.fromString('1')
 export let BI_18 = BigInt.fromI32(18)
+
+// TODO: adjust types
+export function calcLPTokenBorrowedPlusInterest(
+  borrowedInvariant: BigInt,
+  lastCFMMTotalSupply: BigInt,
+  lastCFMMInvariant: BigInt
+): BigInt {
+  if (lastCFMMInvariant != ZERO_BI) {
+    let lpTokenBorrowedPlusInterest = (borrowedInvariant.times(lastCFMMTotalSupply)).div(lastCFMMInvariant)
+    return lpTokenBorrowedPlusInterest
+  }
+
+  return ZERO_BI
+}

a667059193d63f28f284c222cda366f13de157d5
Roberto Martinez <robertomiguel2001@gmail.com>
2022-10-13 20:38:50 -0400

    feat(mappings): implement mapping of update events

---
+++ src/mappings/GammaPool.ts
@@ -0,0 +1,26 @@
+import { PoolUpdated } from '../../generated/GammaPoolFactory/GammaPool'
+import { PoolData as PoolDataSchema } from '../../generated/schema'
+import { GammaPool } from '../../generated/templates/GammaPool/GammaPool'
+import { BigDecimal } from '@graphprotocol/graph-ts'
+
+
+export function handlePoolUpdated(event: PoolUpdated): void {
+  let poolData = new PoolDataSchema(event.transaction.hash.toHex())
+
+  let gammaPool = GammaPool.bind(event.address)
+  let tokenBalances = gammaPool.tokenBalances()
+  let borrowRate = gammaPool.borrowRate()
+
+  poolData.address = event.address // the gammaPool contract address
+  poolData.tokenBalances = [tokenBalances[0].toBigDecimal(), tokenBalances[1].toBigDecimal()] // TODO
+  poolData.lpTokenBalance = BigDecimal.fromString(event.params.lpTokenBalance.toString())
+  poolData.lpTokenBorrowed = BigDecimal.fromString(event.params.lpTokenBalance.toString())
+  poolData.lpTokenBorrowedPlusInterest = BigDecimal.fromString(event.params.lpTokenBorrowedPlusInterest.toString())
+  poolData.lpTokenTotal = poolData.lpTokenBalance.plus(poolData.lpTokenBorrowedPlusInterest)
+  poolData.borrowRate = borrowRate.toBigDecimal()
+  poolData.accFeeIndex = BigDecimal.fromString(event.params.accFeeIndex.toString())
+  poolData.lastFeeIndex = BigDecimal.fromString(event.params.lastFeeIndex.toString())
+  poolData.lastBlockNumber = event.params.lastBlockNumber
+
+  poolData.save()
+}
--- src/mappings/GammaPoolFactory.ts
+++ src/mappings/GammaPoolFactory.ts
@@ -1,44 +1,20 @@
-import { PoolCreated, GammaPoolFactory } from '../../generated/GammaPoolFactory/GammaPoolFactory'
-import { GammaPool as GammaPoolContract } from '../../generated/GammaPoolFactory/GammaPool'
+import { PoolCreated } from '../../generated/GammaPoolFactory/GammaPoolFactory'
+import { GammaPool } from '../../generated/templates'
 import { Pool as PoolCreatedSchema } from '../../generated/schema'
-import { Address, log } from '@graphprotocol/graph-ts'
-import { BigInt, Bytes, ByteArray, crypto, ethereum, bigInt } from '@graphprotocol/graph-ts'
-import { ONE_BI, SIX_BI } from './helpers'
-
-function loadOrCreatePool(id: string): PoolCreatedSchema {
-  const pool = PoolCreatedSchema.load(id)
-  if (pool) return pool
-
-  return new PoolCreatedSchema(id)
-}
+import { BigInt } from '@graphprotocol/graph-ts'
 
 export function handlePoolCreated(event: PoolCreated): void {
-
-  let gammaPool = GammaPoolContract.bind(event.params.pool)
-
-  // loads event details into 
-  const poolCreated = loadOrCreatePool(event.transaction.hash.toHex())
+  // creates new pool instance 
+  const poolCreated = new PoolCreatedSchema(event.transaction.hash.toHex())
 
   poolCreated.address = event.params.pool
   poolCreated.cfmm = event.params.cfmm
-  poolCreated.protocolId = bigInt.fromString(event.params.protocolId.toString())
+  poolCreated.protocolId = BigInt.fromString(event.params.protocolId.toString())
   poolCreated.protocol = event.params.protocol
   poolCreated.count = event.params.count
-  poolCreated.tokenBalances = [ONE_BI, SIX_BI]
-  poolCreated.lpTokenBalance = ONE_BI
-  poolCreated.lpTokenBorrowed = ONE_BI
-  poolCreated.lpTokenBorrowedPlusInterest = ONE_BI
-  poolCreated.lpTokenTotal = ONE_BI
-  poolCreated.borrowRate = ONE_BI
-  poolCreated.accFeeIndex = ONE_BI
-  poolCreated.lastFeeIndex = ONE_BI
-  poolCreated.lastCFMMFeeIndex = ONE_BI
-  poolCreated.lastCFMMInvariant = ONE_BI
-  poolCreated.lastCFMMTotalSupply = ONE_BI
-  poolCreated.lastBlockNumber = ONE_BI
-  poolCreated.totalSupply = ONE_BI
+
+  // instantiate gamma pool template
+  GammaPool.create(event.params.pool)
 
   poolCreated.save()
-}
-
-// handlePoolUpdated()
+} 

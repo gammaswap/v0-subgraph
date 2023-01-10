189610d8c0daf096fa89f6cc1f1304c24cd44a06
fhd <rohithvenkatesan142@gmail.com>
2023-01-09 19:44:28 -0500

    changed abi's, contract addresses, mappings/schema

--- src/mappings/GammaPool.ts
+++ src/mappings/GammaPool.ts
@@ -8,18 +8,18 @@ export function handlePoolUpdated(event: PoolUpdated): void {
   let poolData = new PoolDataSchema(event.transaction.hash.toHex())
 
   let gammaPool = GammaPool.bind(event.address)
-  let tokenBalances = gammaPool.tokenBalances()
-  let borrowRate = gammaPool.borrowRate()
+  //let tokenBalances = gammaPool.tokenBalances()
+  //let borrowRate = gammaPool.borrowRate()
 
   poolData.address = event.address // the gammaPool contract address
-  poolData.tokenBalances = [tokenBalances[0].toBigDecimal(), tokenBalances[1].toBigDecimal()] // TODO
+  //poolData.tokenBalances = [tokenBalances[0].toBigDecimal(), tokenBalances[1].toBigDecimal()] // TODO
   poolData.lpTokenBalance = BigDecimal.fromString(event.params.lpTokenBalance.toString())
   poolData.lpTokenBorrowed = BigDecimal.fromString(event.params.lpTokenBalance.toString())
   poolData.lpTokenBorrowedPlusInterest = BigDecimal.fromString(event.params.lpTokenBorrowedPlusInterest.toString())
   poolData.lpTokenTotal = poolData.lpTokenBalance.plus(poolData.lpTokenBorrowedPlusInterest)
   poolData.lpInvariant = BigDecimal.fromString(event.params.lpInvariant.toString())
   poolData.lpBorrowedInvariant = BigDecimal.fromString(event.params.lpBorrowedInvariant.toString())
-  poolData.borrowRate = borrowRate.toBigDecimal()
+  //poolData.borrowRate = borrowRate.toBigDecimal()
   poolData.accFeeIndex = BigDecimal.fromString(event.params.accFeeIndex.toString())
   poolData.lastFeeIndex = BigDecimal.fromString(event.params.lastFeeIndex.toString())
   poolData.lastBlockNumber = event.params.lastBlockNumber
